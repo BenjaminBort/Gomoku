89df6bb8e37fdfd11b05b190f2b21e970b372359
nadali_t <nadali_t@nadali-t-PC>
2014-10-15 11:19:25 +0200

    woler premier commit

---
+++ Gomoku/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: nadali_t
+ *
+ * Created on October 15, 2014, 11:06 AM
+ */
+
+#include <iostream>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
